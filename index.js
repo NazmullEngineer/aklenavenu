e12ea4648b65e1a384a2cae72b8230c500ab5835
NazmullEngineer <hamida693046@gmail.com>
2020-12-06 03:08:31 +0600

    h

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var postMessageRoutes = require('./controllers/postMessageController')
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
+  
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })

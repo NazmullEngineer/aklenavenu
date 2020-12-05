891bc792daf6ee4fa330641c5465be7c5a49acb6
NazmullEngineer <hamida693046@gmail.com>
2020-12-06 02:42:38 +0600

    gh

--- index.js
+++ index.js
@@ -7,12 +7,7 @@ const path= require('path')
 var postMessageRoutes = require('./controllers/postMessageController')
 
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static('client/build'))
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-//     })
-// }
+
 
 var app = express()
 app.use(bodyParser.json())

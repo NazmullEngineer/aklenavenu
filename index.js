fb01480aca0267100c21aee42642afb4e6bba28b
NazmullEngineer <hamida693046@gmail.com>
2020-12-06 08:07:09 +0600

    g

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ var postMessageRoutes = require('./controllers/postMessageController')
 var app = express()
 app.use(bodyParser.json())
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'))
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//     })
+// }
 app.use(cors({origin:'http://localhost:3000'}))
 app.listen(process.env.PORT || 4000,()=>console.log('Server started at : 4000'))
 
